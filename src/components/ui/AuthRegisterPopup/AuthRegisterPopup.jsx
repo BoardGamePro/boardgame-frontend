c626c5dd03026a773f6aaa0b0a96fa13938fae02
SlavaDin <slavadinonavr@gmail.com>
2025-11-26 12:29:55 +0500

    fixes authorization and registration windows

--- src/components/ui/AuthRegisterPopup/AuthRegisterPopup.jsx
+++ src/components/ui/AuthRegisterPopup/AuthRegisterPopup.jsx
@@ -1,7 +1,7 @@
-import { useTranslations } from 'next-intl'
 import React, { useState } from 'react'
 import Input from '../Input'
 import InputPassword from '../InputPassword'
+import { useTranslations } from 'next-intl'
 import { useRegister } from '@/api/authApi/authApi'
 
 export default function AuthRegisterPopup({
@@ -9,6 +9,7 @@ export default function AuthRegisterPopup({
   handleClosePopup,
 }) {
   const t = useTranslations('registerPopup')
+
   const [userData, setUserData] = useState({
     username: '',
     email: '',
@@ -17,13 +18,11 @@ export default function AuthRegisterPopup({
 
   const registerMutation = useRegister()
 
-  const setData = (data, dataType) => {
-    setUserData((prev) => ({ ...prev, [dataType]: data }))
-  }
+  const setData = (data, type) =>
+    setUserData((prev) => ({ ...prev, [type]: data }))
 
   const handleSubmit = async (evt) => {
     evt.preventDefault()
-
     try {
       await registerMutation.mutateAsync({
         username: userData.username,
@@ -37,39 +36,87 @@ export default function AuthRegisterPopup({
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center">
-      <div className="mt-[55px] mb-[18px] flex w-full flex-col gap-[26px]">
-        <Input
-          placeholder={t('username')}
-          value={userData.username}
-          setValue={(value) => setData(value, 'username')}
-        />
-        <Input
-          placeholder={t('email')}
-          value={userData.email}
-          setValue={(value) => setData(value, 'email')}
-        />
-        <InputPassword
-          placeholder={t('password')}
-          value={userData.password}
-          setValue={(value) => setData(value, 'password')}
-        />
-      </div>
+    <div className="w-full">
+      {/* Title */}
+      <h2 className="h-[25px] text-center text-[18px] font-semibold">
+        Create your account
+      </h2>
+      <p className="mt-[30px] h-[24px] text-center text-[15px] font-normal text-(--color-text-gray)">
+        Join the community and track your favorite games.
+      </p>
 
-      <div className="mb-[29px] flex gap-[5px] text-center">
-        <p className="text-[13px] font-semibold">{t('loginHint')}</p>
-        <button
-          type="button"
-          onClick={changeAuthState}
-          className="text-[13px] font-semibold text-(--color-main) underline hover:no-underline"
-        >
-          {t('linkToLogin')}
+      <form
+        onSubmit={handleSubmit}
+        className="mt-[30px] flex flex-col gap-[30px]"
+      >
+        {/* Username */}
+        <div>
+          <label className="mb-[5px] block h-[17px] text-[14px] font-semibold text-(--foreground)">
+            Username
+          </label>
+          <Input
+            placeholder={t('Enter your username')}
+            value={userData.username}
+            setValue={(value) => setData(value, 'username')}
+            icon="/icons/envelope.svg"
+          />
+        </div>
+
+        {/* Email */}
+        <div>
+          <label className="mb-[5px] block h-[17px] text-[14px] font-semibold text-(--foreground)">
+            Email
+          </label>
+          <Input
+            placeholder={t('Enter your email')}
+            value={userData.email}
+            setValue={(value) => setData(value, 'email')}
+            icon="/icons/envelope.svg"
+          />
+        </div>
+
+        {/* Password */}
+        <div>
+          <label className="mb-[5px] block h-[17px] text-[14px] font-semibold text-(--foreground)">
+            Password
+          </label>
+          <InputPassword
+            placeholder={t('Enter your password')}
+            value={userData.password}
+            setValue={(value) => setData(value, 'password')}
+            icon="/icons/lock.svg"
+          />
+        </div>
+
+        <div>
+          <label className="mb-[5px] block h-[17px] text-[14px] font-semibold text-(--foreground)">
+            Confirm password
+          </label>
+          <InputPassword
+            placeholder={t('Confirm your password')}
+            value={userData.password}
+            setValue={(value) => setData(value, 'password')}
+            icon="/icons/lock.svg"
+          />
+        </div>
+
+        {/* Button */}
+        <button className="flex h-[35px] w-[180px] items-center justify-center self-center rounded-[4px] bg-(--color-accent) px-[16px] py-[4px] text-[14px] leading-[16px] font-medium text-white">
+          {t('signUp')}
         </button>
-      </div>
 
-      <button className="transition-custom min-w-[126px] rounded-[16px] bg-(--color-main) px-[9px] py-[8.5px] text-[20px] font-medium text-white hover:scale-105 active:scale-95">
-        {t('signUp')}
-      </button>
-    </form>
+        {/* Bottom switch */}
+        <div className="h-[24px] text-center text-[14px] text-(--color-text-gray)">
+          Already have an account?{' '}
+          <button
+            type="button"
+            onClick={changeAuthState}
+            className="font-semibold text-(--color-text-default)"
+          >
+            Log in!
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
--- src/components/ui/Input/Input.jsx
+++ src/components/ui/Input/Input.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from 'react'
+import Image from 'next/image'
 
 export default function Input({
   placeholder,
@@ -8,15 +9,28 @@ export default function Input({
   value,
   setValue,
   required = true,
+  icon,
 }) {
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      className="w-full rounded-[15px] border-[2px] border-(--color-gray) bg-white px-[15px] py-[5px] text-[15px] font-bold focus:outline-none"
-      value={value}
-      onChange={(evt) => setValue(evt.target.value)}
-      required={required}
-    />
+    <div className="relative w-full">
+      {icon && (
+        <Image
+          src={icon}
+          alt="icon"
+          width={16}
+          height={16}
+          className="absolute top-1/2 left-[12px] -translate-y-1/2 opacity-70"
+        />
+      )}
+
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={`h-[40px] w-full rounded-[8px] border-[1px] border-(--color-text-light-gray-border) bg-white py-[9px] text-[15px] font-normal focus:outline-none ${icon ? 'pl-[40px]' : 'px-[15px]'}`}
+        value={value}
+        onChange={(evt) => setValue(evt.target.value)}
+        required={required}
+      />
+    </div>
   )
 }
